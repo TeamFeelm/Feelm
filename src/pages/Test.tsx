cb6091244185ff5f663a79c9675c9d6bf56e0eca
Jagyeong Gu <plzkoiu0912@naver.com>
2022-05-26 23:51:40 +0900

    etc: Test pages 끄적임

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,5 +1,28 @@
+/* library import */
 import { useState } from "react";
+import styled from "styled-components";
 
+/* component */
 export default function Test() {
-  
-};
+  const [question, setQuestion] = useState(0);
+  const [answer, setAnswer] = useState(0);
+
+  return (
+    <>
+      <QueDiv bg="grey">
+
+      </QueDiv>
+    </>
+  );
+}
+
+/* props */
+interface props {
+  bg?: string;
+  color?: string;
+}
+
+/* styled-components */
+const QueDiv = styled.div<props>`
+  background-color: ${(props) => props.bg};
+`
