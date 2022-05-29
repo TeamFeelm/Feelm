abfb085f3eff283cfbeb48b441c52a3f52cfba21
hyezo <hyezoprk@kakao.com>
2022-05-30 03:10:47 +0900

    etc: 줄정리..

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,28 @@
     기본 import
 -------------------------------------------*/
 import { useEffect, useState } from "react";
-import "./App.css";
 /*-------------------------------------------
     라이브러리 import
 -------------------------------------------*/
 import { Routes, Route, Link, useNavigate, useParams } from "react-router-dom";
+
 /*-------------------------------------------
     컴퍼넌트 import
 -------------------------------------------*/
-import Detail from './pages/Detail'
-import Test from './pages/Test';
+import { Detail } from "./pages";
+import Test from "./pages/Test";
 /*-------------------------------------------
     App 화면 구성
 -------------------------------------------*/
 export default function App(): JSX.Element {
   return (
     <>
-      {<Detail></Detail>}
+      <Detail></Detail>
       <Test></Test>
 
-      {/* <Routes>
-        <Route path="/" element={
-          <>
-            <Login />
-            <Mine />
-            <Others />
-            <Profile />
-          </>
-        } />
-        <Route path="*" element={<Error />} />
-      </Routes> */}
+      <Routes>
+        <Route path="/" element={<></>} />
+      </Routes>
     </>
   );
 }
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -11,21 +11,21 @@ import TabContent from "../components/tabContent";
   컴퍼넌트
 -------------------------------------------*/
 const Detail = () => {
-  const [tab, setTab] = useState(0)
+  const [tab, setTab] = useState(0);
 
-    return (
-        <>
-          <ImgBox bg="blue">S P S P</ImgBox>
-          <TabBox bg="white">
-            <Tab onClick={ () => setTab(0)}>기본</Tab>
-            <Tab onClick={ () => setTab(1)}>출연</Tab>
-            <Tab onClick={ () => setTab(2)}>줄거리</Tab>
-          </TabBox>
-          <ContentBox>
-            <TabContent tab={tab}    />
-          </ContentBox>
-        </>
-    );
+  return (
+    <>
+      <ImgBox bg="blue">S P S P</ImgBox>
+      <TabBox bg="white">
+        <Tab onClick={() => setTab(0)}>기본</Tab>
+        <Tab onClick={() => setTab(1)}>출연</Tab>
+        <Tab onClick={() => setTab(2)}>줄거리</Tab>
+      </TabBox>
+      <ContentBox>
+        <TabContent tab={tab} />
+      </ContentBox>
+    </>
+  );
 };
 
 /*-------------------------------------------
@@ -46,7 +46,7 @@ const ImgBox = styled.div<props>`
   background: ${(props) => props.bg};
   color: ${(props) => (props.bg === "blue" ? "white" : "black")};
   font-size: 100px;
-`
+`;
 const TabBox = styled.div<props>`
   padding-top: 50px;
   padding-bottom: 10px;
@@ -55,15 +55,15 @@ const TabBox = styled.div<props>`
   background: ${(props) => props.bg};
   color: ${(props) => (props.bg === "white" ? "black" : "white")};
   border-bottom: 1px solid black;
-`
+`;
 const ContentBox = styled.div`
   padding: 20px;
   margin: auto;
   width: 60%;
-`
+`;
 const Tab = styled.span`
   padding: 20px;
   margin-right: 30px;
-`
+`;
 
 export default Detail;
