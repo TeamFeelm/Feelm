adba7a70d617dca4b8016a26904613e4d97bfc9c
hyezo <hyezoprk@kakao.com>
2022-06-08 04:18:39 +0900

    etc: 개행

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,14 @@ export default function Home() {
 
   return (
     <>
+      <img
+        src="https://movie.naver.com/movie/bi/mi/photoViewPopup.naver?movieCode=29268"
+        alt=""
+      />
       {movieList.map((a, i) => {
         return (
           <div key={a.id}>
+            <img src={a.img} alt="" />
             <h4>{a.title}</h4>
           </div>
         );
