f04c6134222ce0bd3d893a11c6901cc6a008aaca
우재윤 <iwytbbtss00@naver.com>
2022-06-13 17:07:38 +0900

    feat: slick shuffle, design: mainBox

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,26 @@
 import { MovieSlide } from "../components";
+import styled from "styled-components";
 
 export default function Home() {
   return (
     <>
+      <Box color="lightblue">ㅆㅣㅈㅏㅇ</Box>
+      <Box color="lightgreen">ㅆㅣㅈㅏㅇ</Box>
       <MovieSlide></MovieSlide>
+      <Box color="orange">ㅆㅣㅈㅏㅇ</Box>
+      <Box color="lightyellow">ㅆㅣㅈㅏㅇ</Box>
     </>
   );
 }
+
+interface color {
+  color: string;
+}
+
+const Box = styled.div<color>`
+  width: 100vw;
+  height: 800px;
+  background-color: ${(props) => props.color};
+  font-size: 50px;
+  text-align: center;
+`;
