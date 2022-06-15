8b3f7ca72b3215bf7a444af24ebcb0955f1d72a8
jagyeong gu <plzkoiu0912@naver.com>
2022-06-15 09:18:30 +0900

    design: globalstyle main 수정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -81,11 +81,6 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   main {
-    position: relative;
-    display: flex;
-    flex-direction:column;
-    align-items: center;
-    justify-content: center;
     margin-top: 50px;
   }
 
